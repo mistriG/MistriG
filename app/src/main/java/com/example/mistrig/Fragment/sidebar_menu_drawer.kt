5b82b2eda6b3183320ed1b936c8cb188710049c8
NeerAtHorizone <neerajsharma198118131@gmail.com>
2025-03-31 15:41:38 +0530

    messages onClick to open - message box

--- app/src/main/java/com/example/mistrig/Fragment/sidebar_menu_drawer.kt
+++ app/src/main/java/com/example/mistrig/Fragment/sidebar_menu_drawer.kt
@@ -1,12 +1,14 @@
 package com.example.mistrig.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.mistrig.R
-
+import kotlin.jvm.java
 
 
 class sidebar_menu_drawer : Fragment() {
@@ -20,7 +22,13 @@ class sidebar_menu_drawer : Fragment() {
 
         val view : View = inflater.inflate(R.layout.fragment_sidebar_menu_drawer, container, false)
 
+        val msgBtn : ImageView = view.findViewById<ImageView>(R.id.btn_show_msg)
 
+        val msgBoxIntent = Intent(view.context, MessageBox::class.java)
+
+        msgBtn.setOnClickListener {
+            startActivity(msgBoxIntent)
+        }
 
         return view
     }
