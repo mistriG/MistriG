ed0c0692338fcacc7115a1f496b1c0f3887b3f13
NeerAtHorizone <neerajsharma198118131@gmail.com>
2025-04-02 21:40:44 +0530

    splash screen animation added

--- app/src/main/java/com/example/mistrig/Activity/MainActivity.kt
+++ app/src/main/java/com/example/mistrig/Activity/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.mistrig.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mistrig.R
@@ -16,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val animation: android.view.animation.Animation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.translate_up)
+        val image = findViewById<ImageView>(R.id.activity_main_logo)
+        image.startAnimation(animation)
         // Redirect to Login activity after a delay
         Handler().postDelayed({
             val mainIntent = Intent(this@MainActivity, Login::class.java)
