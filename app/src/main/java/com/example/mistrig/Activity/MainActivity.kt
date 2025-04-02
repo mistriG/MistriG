886203d37c30c01491961564b2675a70919f3f7b
NeerAtHorizone <neerajsharma198118131@gmail.com>
2025-04-02 21:45:54 +0530

    splash screen animation added - refactor

--- app/src/main/java/com/example/mistrig/Activity/MainActivity.kt
+++ app/src/main/java/com/example/mistrig/Activity/MainActivity.kt
@@ -18,9 +18,10 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val animation: android.view.animation.Animation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.translate_up)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.translate_up)
         val image = findViewById<ImageView>(R.id.activity_main_logo)
         image.startAnimation(animation)
+
         // Redirect to Login activity after a delay
         Handler().postDelayed({
             val mainIntent = Intent(this@MainActivity, Login::class.java)
