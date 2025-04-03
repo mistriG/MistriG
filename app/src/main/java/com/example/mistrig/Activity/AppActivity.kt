dc29c9156cf294271f2c67d977dda0716f1269e6
harsh <harshvardhansingh2609@gmail.com>
2025-04-03 12:35:19 +0530

    cosmetic changes

--- app/src/main/java/com/example/mistrig/Activity/AppActivity.kt
+++ app/src/main/java/com/example/mistrig/Activity/AppActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
@@ -21,6 +22,7 @@ class AppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         setContentView(R.layout.activity_app)
 
 
