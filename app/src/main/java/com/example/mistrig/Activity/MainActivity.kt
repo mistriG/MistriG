0b9047ef3c7cce1cd29963057af1f3a0856b3e84
MistriG <mistrigpvtltd@gmail.com>
2025-05-14 23:54:27 +0530

    Added text animation to splash screen and updated app icon

--- app/src/main/java/com/example/mistrig/Activity/MainActivity.kt
+++ app/src/main/java/com/example/mistrig/Activity/MainActivity.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mistrig.R
 
 class MainActivity : AppCompatActivity() {
-    private val SPLASH_DISPLAY_LENGTH = 500
+    private val SPLASH_DISPLAY_LENGTH = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +19,20 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val animation = AnimationUtils.loadAnimation(this, R.anim.translate_up)
+        // Animate logo from bottom to center
+        val logoAnimation = AnimationUtils.loadAnimation(this, R.anim.translate_up)
         val image = findViewById<ImageView>(R.id.activity_main_logo)
-        image.startAnimation(animation)
+        image.startAnimation(logoAnimation)
+        
+        // Animate "Mistri" from left to center
+        val leftToCenter = AnimationUtils.loadAnimation(this, R.anim.slide_left_to_center)
+        val textMistri = findViewById<TextView>(R.id.text_mistri)
+        textMistri.startAnimation(leftToCenter)
+        
+        // Animate "G" from right to center
+        val rightToCenter = AnimationUtils.loadAnimation(this, R.anim.slide_right_to_center)
+        val textG = findViewById<TextView>(R.id.text_g)
+        textG.startAnimation(rightToCenter)
 
         // Redirect to Login activity after a delay
         Handler().postDelayed({
